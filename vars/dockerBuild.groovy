49907dc056b836d5461bdfc8a7a12c386f7af0e1
Dummy Robot <myatsoeaumg@hotmail.com>
2025-06-29 18:55:04 +0630

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,14 @@
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def context = config.context ?: '.'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile} in ${context}"
+    
+    dir(context) {
+        sh """
+            docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} .
+        """
+    }
+}
