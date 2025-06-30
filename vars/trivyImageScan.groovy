e58c11357970da8d1613939d7d026b1b9976e2c4
Dummy Robot <myatsoeaumg@hotmail.com>
2025-06-30 11:20:28 +0630

    Update trivyImageScan.groovy

--- vars/trivyImageScan.groovy
+++ vars/trivyImageScan.groovy
@@ -1,3 +1,13 @@
-def call(String imageName) {
-    sh "trivy image --format table ${imageName}"
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Docker image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def outputFile = config.outputFile ?: "${imageName.replaceAll('/', '_')}-${imageTag}-trivy-image-report.html"
+
+    echo "🔍 Scanning Docker image: ${imageName}:${imageTag} with Trivy"
+
+    sh """
+        trivy image --format html -o ${outputFile} ${imageName}:${imageTag}
+    """
+
+    echo "📄 Trivy scan report generated: ${outputFile}"
 }
