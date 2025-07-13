cdc168d32611e6f0713e0c34727f76962b46cd74
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-13 10:48:55 +0630

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,12 @@
-def call(String projectName, String projectKey) {
-    withSonarQubeEnv('sonar') {
-        sh """${tool('sonar-scanner')}/bin/sonar-scanner \
-            -Dsonar.projectName=${projectName} \
-            -Dsonar.projectKey=${projectKey}"""
+def call(String projectKey, String projectName, String projectVersion = '1.0') {
+    withSonarQubeEnv('SonarServer') {
+        sh """
+            sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.projectName=${projectName} \
+                -Dsonar.projectVersion=${projectVersion} \
+                -Dsonar.sources=. \
+                -Dsonar.javascript.node.maxspace=4096
+        """
     }
 }
