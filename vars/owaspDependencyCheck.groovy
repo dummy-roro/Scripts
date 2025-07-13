bbe82b29bea54e4cf45e2506cfe4c7ee5a0edd3c
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-13 10:35:31 +0630

    Update owaspDependencyCheck.groovy

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,13 +1,22 @@
 def call(List<String> dirs = []) {
-    dirs.each { dir ->
-        echo "Running OWASP Dependency Check in ${dir}"
-        dependencyCheck additionalArguments: """
-            --scan ./${dir} 
-            --out ./reports/${dir}
-            --format ALL
-            --prettyPrint
-        """, odcInstallation: 'OWASP-DepCheck-10'
-
-        dependencyCheckPublisher pattern: "reports/${dir}/dependency-check-report.xml", failedTotalCritical: 1
+    if (dirs.isEmpty()) {
+        echo "Running OWASP Dependency Check in root directory"
+        runDependencyCheck('.', 'root')
+    } else {
+        dirs.each { dir ->
+            echo "Running OWASP Dependency Check in ${dir}"
+            runDependencyCheck("./${dir}", dir)
+        }
     }
+}
+
+private void runDependencyCheck(String scanPath, String reportName) {
+    dependencyCheck additionalArguments: """
+        --scan ${scanPath}
+        --out ./reports/${reportName}
+        --format ALL
+        --prettyPrint
+    """, odcInstallation: 'OWASP-DepCheck-10'
+
+    dependencyCheckPublisher pattern: "reports/${reportName}/dependency-check-report.xml", failedTotalCritical: 1
 }
