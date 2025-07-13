2d395b94648ea1c98cc0a83f059205a38cbe8d5f
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-13 10:31:52 +0630

    Update npmAudit.groovy

--- vars/npmAudit.groovy
+++ vars/npmAudit.groovy
@@ -1,4 +1,13 @@
-def call() {
-    sh 'npm install --legacy-peer-deps'
-    sh 'npm audit --audit-level=critical'
+def call(List<String> dirs = []) {
+    if (dirs.isEmpty()) {
+        echo "Running npm audit in root"
+        sh 'npm install --legacy-peer-deps'
+        sh 'npm audit --audit-level=critical'
+    } else {
+        dirs.each { dir ->
+            echo "Running npm audit in ${dir}"
+            sh "npm --prefix ${dir} install --legacy-peer-deps"
+            sh "npm --prefix ${dir} audit --audit-level=critical"
+        }
+    }
 }
