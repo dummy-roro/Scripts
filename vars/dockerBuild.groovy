aa75bcf7c81ee45008d5948568aeee1fd99f03f8
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-21 21:27:38 +0630

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,18 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def context = config.context ?: '.'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def imageName = config.imageName ?: error('Image name is required')
+    def imageTag  = config.imageTag  ?: 'latest'
+    def context   = config.context   ?: '.'
+    def dockerfile= config.dockerfile?: 'Dockerfile'
 
-    echo "Building Docker image: ${imageName}:${imageTag} using Dockerfile '${dockerfile}' and context '${context}'"
+    echo "Building Docker image: ${imageName}:${imageTag} (Dockerfile: ${dockerfile}, context: ${context})"
 
     sh """
-        docker build -t "${imageName}:${imageTag}" -t "${imageName}:latest" -f "${dockerfile}" "${context}"
+        docker build \
+          --pull \
+          --no-cache \
+          -t "${imageName}:${imageTag}" \
+          -t "${imageName}:latest" \
+          -f "${dockerfile}" \
+          "${context}"
     """
 }
